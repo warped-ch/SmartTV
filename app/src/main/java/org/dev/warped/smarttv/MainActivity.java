09bb3240ecdcfdbb26782ba66ffed4024fc0f231
Andreas Wiedmer <wiedmer.a@gmail.com>
2016-06-19 14:28:47 +0200

    % add Bouquets and Settings to navigation drawer, remove other items

--- app/src/main/java/org/dev/warped/smarttv/MainActivity.java
+++ app/src/main/java/org/dev/warped/smarttv/MainActivity.java
@@ -74,24 +74,15 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_bouquets) {
+            // TODO, anwi
+        } else if (id == R.id.nav_settings) {
+            // TODO, anwi
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
