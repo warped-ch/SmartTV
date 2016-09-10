2903109ec9f52c00ef60642dcf0e875cf2715362
Andreas Wiedmer <wiedmer.a@gmail.com>
2016-09-10 19:53:40 +0200

    + extend ResourceUtilTest, delete unused ApplicationTest

--- app/src/test/java/org/dev/warped/smarttv/ResourceUtilTest.java
+++ app/src/test/java/org/dev/warped/smarttv/ResourceUtilTest.java
@@ -17,14 +17,26 @@ public class ResourceUtilTest {
     }
 
     @Test
-    public void getReceiverType_eEnigma2() throws Exception {
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox7025"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox500HD"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox800HDPVR"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox800HDse"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox820HD"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox8000HDPVR"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox7020HD"));
-        assertEquals(ReceiverClient.EReceiverType.eEnigma2, ResourceUtil.getReceiverType("Dreambox7080HD"));
+    public void getReceiverType() throws Exception {
+        assertEquals(ReceiverClient.EReceiverType.eDM500HD, ResourceUtil.getReceiverType("dm500hd"));
+        assertEquals(ReceiverClient.EReceiverType.eDM500HDV2, ResourceUtil.getReceiverType("dm500hdv2"));
+        assertEquals(ReceiverClient.EReceiverType.eDM800, ResourceUtil.getReceiverType("dm800"));
+        assertEquals(ReceiverClient.EReceiverType.eDM800SE, ResourceUtil.getReceiverType("dm800se"));
+        assertEquals(ReceiverClient.EReceiverType.eDM800SEV2, ResourceUtil.getReceiverType("dm800sev2"));
+        assertEquals(ReceiverClient.EReceiverType.eDM7020HD, ResourceUtil.getReceiverType("dm7020hd"));
+        assertEquals(ReceiverClient.EReceiverType.eDM7020HDV2, ResourceUtil.getReceiverType("dm7020hdv2"));
+        assertEquals(ReceiverClient.EReceiverType.eDM8000, ResourceUtil.getReceiverType("dm8000"));
+    }
+
+    @Test
+    public void getReceiverTypeValue() throws Exception {
+        assertEquals("dm500hd", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM500HD));
+        assertEquals("dm500hdv2", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM500HDV2));
+        assertEquals("dm800", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM800));
+        assertEquals("dm800se", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM800SE));
+        assertEquals("dm800sev2", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM800SEV2));
+        assertEquals("dm7020hd", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM7020HD));
+        assertEquals("dm7020hdv2", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM7020HDV2));
+        assertEquals("dm8000", ResourceUtil.getReceiverTypeValue(ReceiverClient.EReceiverType.eDM8000));
     }
 }
