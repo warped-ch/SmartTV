76a862bebbba55ae3638cb4e183b41354f8dce08
Andreas Wiedmer <wiedmer.a@gmail.com>
2019-06-12 11:06:08 +0200

    - use MulticastLock to get permission to receive Wifi Multicast packets for Network Service Discovery
    https://stackoverflow.com/questions/53615125/nsdmanager-discovery-does-not-work-on-android-9
    
    https://play.google.com/store/apps/details?id=org.dev.warped.smarttv&reviewId=gp%3AAOqpTOF26cPBOBuSNpmK_25ft--QI-Hmh5WDDwI4n1K7vdQFLHZ_1EYgSaZEzt6BXmObz7obVZRecInGVWUGKxI

--- app/src/main/java/org/dev/warped/smarttv/DeviceDiscovery.java
+++ app/src/main/java/org/dev/warped/smarttv/DeviceDiscovery.java
@@ -3,6 +3,7 @@ package org.dev.warped.smarttv;
 import android.content.Context;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
+import android.net.wifi.WifiManager;
 
 import java.util.ArrayList;
 
@@ -10,18 +11,26 @@ import timber.log.Timber;
 
 class DeviceDiscovery {
     private final DeviceDiscoveryCallback mCallback;
+    /** MulticastLock to get permission to receive Wifi Multicast packets for Network Service Discovery. */
+    private final WifiManager.MulticastLock mMulticastLock;
     private final NsdManager mNsdManager;
     private final Enigma2DiscoveryListener mEnigma2DiscoveryListener;
     private final ArrayList<Device> mDevices = new ArrayList<>();
 
     public DeviceDiscovery(DeviceDiscoveryCallback callback, Context context) {
         mCallback = callback;
+
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        mMulticastLock = wifiManager.createMulticastLock("DeviceDiscovery::startDiscovery");
+        mMulticastLock.setReferenceCounted(true);
+
         mNsdManager = (NsdManager) context.getSystemService(Context.NSD_SERVICE);
         mEnigma2DiscoveryListener = new Enigma2DiscoveryListener(this, mNsdManager);
     }
 
     public void startDiscovery() {
         Timber.d("startDiscovery: ");
+        mMulticastLock.acquire();
         mNsdManager.discoverServices(
                 mEnigma2DiscoveryListener.getServiceType(), NsdManager.PROTOCOL_DNS_SD, mEnigma2DiscoveryListener);
     }
@@ -29,6 +38,8 @@ class DeviceDiscovery {
     public void stopDiscovery() {
         Timber.d("stopDiscovery: ");
         mNsdManager.stopServiceDiscovery(mEnigma2DiscoveryListener);
+        mMulticastLock.release();
+
         mDevices.clear();
     }
 
